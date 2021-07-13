08d4d9bd12d0dfa05e1538dd4e907e2618640fa3
Koushik Banerjee <81717986+koushikop5@users.noreply.github.com>
2021-07-13 12:58:20 +0530

    Update disconnect.js

--- commands/Song/disconnect.js
+++ commands/Song/disconnect.js
@@ -21,19 +21,10 @@ module.exports = {
       if (!player.queue || !player.queue.current) return message.channel.send(`**:x: Nothing playing in this server**`);
       //if user is not in the right channel as bot, then return error
       if(player && channel.id !== player.voiceChannel)
-        return message.channel.send(`**:x: You need to be in the same voice channel as Milrato x Rythm to use this command**`);
+        return message.channel.send(`**:x: You need to be in the same voice channel as Discord to use this command**`);
       //clear the QUEUE
       player.destroy();
       //Send Success Message
       return message.channel.send(`**:mailbox_with_no_mail: Successfully disconnected**`);
   }
 };
-/**
- * @INFO
- * Bot Coded by Tomato#6966 | https://github.com/Tomato6966/discord-js-lavalink-Music-Bot-erela-js
- * @INFO
- * Work for Milrato Development | https://milrato.eu
- * @INFO
- * Please mention Him / Milrato Development, when using this Code!
- * @INFO
- */
