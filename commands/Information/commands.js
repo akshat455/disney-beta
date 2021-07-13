4ba57672cb082d6e314db4a3d1e6e36087b8209c
Koushik Banerjee <81717986+koushikop5@users.noreply.github.com>
2021-07-13 12:49:32 +0530

    Update commands.js

--- commands/Information/commands.js
+++ commands/Information/commands.js
@@ -64,12 +64,4 @@ let string3 = `\`\`\`fix
     }
   }
 }
-/**
-  * @INFO
-  * Bot Coded by Tomato#6966 | https://github.com/Tomato6966/Discord-Js-Handler-Template
-  * @INFO
-  * Work for Milrato Development | https://milrato.eu
-  * @INFO
-  * Please mention Him / Milrato Development, when using this Code!
-  * @INFO
-*/
+
