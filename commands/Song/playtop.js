5e8b6fd4ac63d83b10a0466bfb5cb4044b50286e
Koushik Banerjee <81717986+koushikop5@users.noreply.github.com>
2021-07-13 13:10:41 +0530

    Update playtop.js

--- commands/Song/playtop.js
+++ commands/Song/playtop.js
@@ -34,7 +34,7 @@ module.exports = {
     const player = client.manager.players.get(message.guild.id);
     //if user is not in the right channel as bot, then return error
     if (player && channel.id !== player.voiceChannel)
-      return message.channel.send(`**:x: You need to be in the same voice channel as Milrato x Rythm to use this command**`);
+      return message.channel.send(`**:x: You need to be in the same voice channel as  to use this command**`);
     //if bot connected bot not with the lavalink player then try to delete the player
     if (player && botchannel && channel.id !== botchannel.id) {
       player.destroy();
@@ -57,7 +57,7 @@ module.exports = {
       message.channel.send(`<:soundcloud:818555972079321128> **Searching** :mag_right: \`${args.join(" ")}\``)
       //play the song from our playermanager
       playermanager(client, message, args, `play:soundcloud`);
-      //ELSE SEND RYTHM INFO
+      //ELSE SEND INFO
     } else if (message.content.includes("http")) {
       //send searching
       message.channel.send(`<:rythm:826519647347539990> **Searching** :mag_right: \`${args.join(" ")}\``)
@@ -71,12 +71,4 @@ module.exports = {
     }
   }
 };
-/**
- * @INFO
- * Bot Coded by Tomato#6966 | https://github.com/Tomato6966/discord-js-lavalink-Music-Bot-erela-js
- * @INFO
- * Work for Milrato Development | https://milrato.eu
- * @INFO
- * Please mention Him / Milrato Development, when using this Code!
- * @INFO
- */
+
